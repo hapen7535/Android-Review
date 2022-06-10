2083212bf9e25782d91a0d23deace5ad7aea09fa
Yuhee Kim <dbgml4509@naver.com>
2022-06-10 23:16:40 +0900

    Handler, 생명 주기 실습 코드

---
+++ app/src/main/java/com/example/reviewumc/HandlerActivity.kt
@@ -0,0 +1,11 @@
+package com.example.reviewumc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HandlerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_handler)
+    }
+}
